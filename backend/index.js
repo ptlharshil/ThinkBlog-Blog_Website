01e058e68405b08a5f946d1de7788063f6e77004
ptlharshil <patelharshil3695@gmail.com>
2021-07-11 15:26:01 -0400

    CRUD for posts and users

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,9 @@ const dotenv=require("dotenv");
 const mongoose=require("mongoose");
 const authRoute=require("./routes/auth")
 const userRoute=require("./routes/users")
+const postRoute=require("./routes/posts")
+const categoryRoute=require("./routes/categories")
+const multer =require("multer")
 dotenv.config();
 
 app.use(express.json());
@@ -14,8 +17,23 @@ mongoose.connect(process.env.MONGO_URL,
     useCreateIndex:true,
     }).then(console.log("Connected MongoDB")).catch((err)=>console.log(err));
 
+    const storage=multer.diskStorage({
+        destination:(req,file,cb)=>{
+            cb(null,"images");
+        },filename:(req,file,cb)=>{
+            cb(null,req.body.name);
+        }
+    });
+
+    const upload=multer({storage:storage})
+    app.post("/api/upload", upload.single("file"),(req,res)=>{
+        res.status(200).json("File Uploaded");
+    })
+
     app.use("/api/auth",authRoute);
     app.use("api/users",userRoute);
+    app.use("/api/posts",postRoute);
+    app.use("/api/categories",categoryRoute);
 
 app.listen("5000", ()=>{
     console.log("Backend is running")
